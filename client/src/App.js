084a5abe4a54266fbc8dc26c3e19ded3628cfd75
Jai chouhan <92376924+Jaichouhan@users.noreply.github.com>
2022-08-04 15:35:35 +0530

    api implementation

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,41 @@
+import Loader from "./Components/Loader";
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <form className="col s12">
+          <div className="row">
+            <div className="input-field col s6">
+              <i className="material-icons prefix">account_circle</i>
+              <input id="icon_prefix" type="text" className="validate" />
+              <label htmlFor="icon_prefix">Title</label>
+            </div>
+            <div className="input-field col s6">
+              <i className="material-icons prefix">description</i>
+              <input id="description" type="tel" className="validate" />
+              <label htmlFor="description">content</label>
+            </div>
+          </div>
+        </form>
+      </div>
+      <Loader />
+      <div className="collection">
+        <a href="#!" className="collection-item">
+          Alvin
+        </a>
+        <a href="#!" className="collection-item active">
+          Alvin
+        </a>
+        <a href="#!" className="collection-item">
+          Alvin
+        </a>
+        <a href="#!" className="collection-item">
+          Alvin
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default App;
